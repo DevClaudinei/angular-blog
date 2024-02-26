d7b5fce3d698377c6aa1646ad89539d6ffe3da23
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-25 21:54:23 -0300

    feat: criando mock de informacoes para componentes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "ADQUIRA OS NOVOS MODELOS DE BIKE AQUI NA SOUL CYCLES.",
+    "description": "A Soul Cycles garante componentes de alto padrão e constante inovação em seus quadros, acabamentos e grafismos.",
+    "photoCover": "https://image.isu.pub/141202135656-b967eec92f9dd8c0e4bc056d692d894e/jpg/page_1.jpg"
+  },
+  {
+    "id": "2",
+    "title": "SL329 - Canastra Boost SX Suntour",
+    "description": "Mountain Bike",
+    "photoCover": "https://soulcycles.vtexassets.com/arquivos/ids/183402-460-460?v=638098232418630000&width=460&height=460&aspect=true"
+  },
+  {
+    "id": "3",
+    "title": "3R5 Team Rival AXS Rodas Brave Carbono",
+    "description": "Gravel Bike",
+    "photoCover": "https://proimport.vtexassets.com/arquivos/ids/183154-460-460?v=638055969376970000&width=460&height=460&aspect=true"
+  },
+  {
+    "id": "4",
+    "title": "SPRY Expedition Urban Microshift",
+    "description": "Road Bike",
+    "photoCover": "https://proimport.vtexassets.com/arquivos/ids/186091-800-571?v=638386948964200000&width=800&height=571&aspect=true"
+  }
+]
